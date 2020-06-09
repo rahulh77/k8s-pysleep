d73cc705b52ca17462ef1d171c97a9a0479e1b8a
Rahul <rahulmodi@Rahuls-MacBook-Pro.local>
2020-06-09 04:33:05 -0400

    cleanup

--- task.py
+++
@@ -1,8 +0,0 @@
-import dataclasses
-
-
-@dataclasses.dataclass
-#@dataclasses.dataclass()                                       # alternative
-class Task:
-    task_name : str
-    sleep_time : int = 0
